527404b840da8b2797249d02e619d69bde486ba2
mzrtamp <mozartrafi@stegripe.org>
2024-07-19 09:21:05 +0700

    chore(config): add devs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@ import type { ColorResolvable, ShardingManagerMode, UserResolvable } from "disco
 
 export const shardingMode: ShardingManagerMode = "worker";
 export const shardsCount: number | "auto" = "auto";
-export const devs: UserResolvable[] = ["319872685897416725"];
+export const devs: UserResolvable[] = ["319872685897416725", "366169273485361153"];
 export const devGuild: string[] = [];
 export const embedColor = "4B7EFF" as ColorResolvable;
 export const defaultPrefix = "-";
