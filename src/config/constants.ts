501f958dede36a0bae52a09b4395cc7c905ed8d0
Syah Warid Ghani Akram <syahwarid404@gmail.com>
2026-09-19 19:31:16 +0700

    fix(env): update example developer IDs in .env.example and constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,5 @@ import process from "node:process";
 import { type UserResolvable } from "discord.js";
 
 export const defaultPrefix = "!";
-export const defaultDevs: UserResolvable[] = ["319872685897416725", "366169273485361153"];
+export const defaultDevs: UserResolvable[] = ["956162927726063626", "366169273485361153"];
 export const enableSharding: boolean = process.env.ENABLE_SHARDING !== "no";
